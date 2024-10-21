3347698d0da40bd28da5541eb647df2a06531b85
Kaitozerob <u202223781@upc.edu.pe>
2024-10-21 17:56:13 -0500

    feat: Add routes for vehicle and category management with validation for arguments

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:front_ruedarent_flutter/src/data/models/user_model.dart';
+import 'package:front_ruedarent_flutter/src/data/models/vehicle_model.dart';
+import 'package:front_ruedarent_flutter/src/data/models/vehicle_type_model.dart';
 import 'package:front_ruedarent_flutter/src/presentation/pages/auth/login/LoginBlocCubit.dart';
 import 'package:front_ruedarent_flutter/src/presentation/pages/auth/login/LoginPage.dart';
 import 'package:front_ruedarent_flutter/src/presentation/pages/auth/passwordRecovery/PasswordRecoveryPage.dart';
 import 'package:front_ruedarent_flutter/src/presentation/pages/auth/register/RegisterBlocCubit.dart';
 import 'package:front_ruedarent_flutter/src/presentation/pages/auth/register/RegisterPage.dart';
+import 'package:front_ruedarent_flutter/src/presentation/pages/owner/vehicles/AddCategoryPage.dart';
+import 'package:front_ruedarent_flutter/src/presentation/pages/owner/vehicles/EditCategoryPage.dart';
+import 'package:front_ruedarent_flutter/src/presentation/pages/owner/vehicles/VehiclesPage.dart';
+import 'package:front_ruedarent_flutter/src/presentation/pages/owner/vehicles/vehicle/AddVehiclePage.dart';
+import 'package:front_ruedarent_flutter/src/presentation/pages/owner/vehicles/vehicle/EditVehiclePage.dart';
 import 'package:front_ruedarent_flutter/src/presentation/pages/roles/RolesPage.dart';
 
 
@@ -44,6 +51,32 @@ class MyApp extends StatelessWidget {
           },
           '/password-recovery': (context) => PasswordRecoveryPage(),
           // Agrega otras rutas si es necesario...
+          '/vehicles-owner': (context) => const VehiclesPage(),
+          '/add-vehicle': (context) {
+            final args = ModalRoute.of(context)!.settings.arguments;
+            if (args is Map<String, dynamic> && args.containsKey('vehicleTypeId')) {
+              return AddVehiclePage(vehicleTypeId: args['vehicleTypeId'] as int);
+            } else {
+              return const Scaffold(body: Center(child: Text('Error: argumentos inválidos')));
+            }
+          },
+          '/edit-vehicle': (context) {
+            final vehicle = ModalRoute.of(context)!.settings.arguments;
+            if (vehicle is VehicleModel) {
+              return EditVehiclePage(vehicle: vehicle);
+            } else {
+              return const Scaffold(body: Center(child: Text('Error: argumentos inválidos')));
+            }
+          },
+          '/edit-category-vehicle': (context) {
+            final args = ModalRoute.of(context)!.settings.arguments;
+            if (args is VehicleTypeModel) {
+              return EditCategoryPage(vehicleType: args);
+            } else {
+              return const Scaffold(body: Center(child: Text('Error: argumentos inválidos')));
+            }
+          },
+          '/add-category-vehicle': (context) => AddCategoryPage(),
         },
       ),
     );
