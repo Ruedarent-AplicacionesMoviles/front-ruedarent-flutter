d9219a0733dfb233b30eac7ac0f5d2416981f6df
Kaitozerob <u202223781@upc.edu.pe>
2024-10-22 01:09:05 -0500

    feat: integrate search functionality with vehicle categories and add bottom navigation bar

--- lib/src/presentation/pages/renter/RentadorVehiclesPage.dart
+++ lib/src/presentation/pages/renter/RentadorVehiclesPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:front_ruedarent_flutter/src/data/repositories/vehicle_type_repository.dart';
 import 'package:front_ruedarent_flutter/src/data/models/vehicle_type_model.dart';
-import 'package:front_ruedarent_flutter/src/presentation/pages/renter/category/CategoryVehiclesForRenterPage.dart'; // Asegúrate de importar correctamente la pantalla
+import 'package:front_ruedarent_flutter/src/presentation/pages/renter/category/CategoryVehiclesForRenterPage.dart';
 
 class RentadorVehiclesPage extends StatefulWidget {
   const RentadorVehiclesPage({super.key});
@@ -12,6 +12,8 @@ class RentadorVehiclesPage extends StatefulWidget {
 
 class _RentadorVehiclesPageState extends State<RentadorVehiclesPage> {
   List<VehicleTypeModel> vehicleTypes = [];
+  String _searchTerm = ''; // Variable para manejar el término de búsqueda
+  int _selectedIndex = 0; // Índice de la página seleccionada en la barra
 
   @override
   void initState() {
@@ -27,8 +29,35 @@ class _RentadorVehiclesPageState extends State<RentadorVehiclesPage> {
     });
   }
 
+  // Manejar la navegación al cambiar de índice en la barra de navegación
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    // Manejar la navegación entre las pantallas según el índice
+    switch (index) {
+      case 0:
+      // Mantenerse en la pantalla actual
+        break;
+      case 1:
+      // Navegar a la pantalla de alquiler de vehículos
+        Navigator.pushReplacementNamed(context, '/rent');
+        break;
+      case 2:
+      // Navegar a la pantalla de perfil del usuario
+        Navigator.pushReplacementNamed(context, '/profile');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Filtrar los resultados según el término de búsqueda
+    List<VehicleTypeModel> filteredVehicleTypes = vehicleTypes
+        .where((type) => type.name.toLowerCase().contains(_searchTerm.toLowerCase()))
+        .toList();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Vehículos Disponibles'),
@@ -37,73 +66,111 @@ class _RentadorVehiclesPageState extends State<RentadorVehiclesPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: vehicleTypes.isNotEmpty
-            ? ListView.builder(
-          itemCount: vehicleTypes.length,
-          itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () {
-                // Al hacer clic, navega a la página de vehículos en esa categoría
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CategoryVehiclesForRenterPage(
-                      categoryName: vehicleTypes[index].name, // Pasar el nombre de la categoría
-                    ),
-                  ),
-                );
+        child: Column(
+          children: [
+            // Campo de búsqueda
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Buscar vehículos',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _searchTerm = value;
+                });
               },
-              child: Card(
-                margin: const EdgeInsets.symmetric(vertical: 10.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image(
-                        image: vehicleTypes[index].image != null &&
-                            vehicleTypes[index].image!.isNotEmpty
-                            ? NetworkImage(vehicleTypes[index].image!)
-                            : const AssetImage(
-                            'assets/images/vehicles/default.png'),
-                        height: 100,
-                        width: 100,
-                        fit: BoxFit.cover,
-                      ),
-                      const SizedBox(width: 16),
-                      Expanded(
-                        child: Column(
+            ),
+            const SizedBox(height: 20),
+            // Mostrar resultados filtrados
+            Expanded(
+              child: filteredVehicleTypes.isNotEmpty
+                  ? ListView.builder(
+                itemCount: filteredVehicleTypes.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CategoryVehiclesForRenterPage(
+                            categoryName: filteredVehicleTypes[index].name,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      margin: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              vehicleTypes[index].name,
-                              style: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.green,
-                              ),
+                            Image(
+                              image: filteredVehicleTypes[index].image != null &&
+                                  filteredVehicleTypes[index].image!.isNotEmpty
+                                  ? NetworkImage(filteredVehicleTypes[index].image!)
+                                  : const AssetImage('assets/images/vehicles/default.png'),
+                              height: 100,
+                              width: 100,
+                              fit: BoxFit.cover,
                             ),
-                            const SizedBox(height: 8),
-                            Text(
-                              vehicleTypes[index].info ?? '',
-                              style: const TextStyle(fontSize: 16),
+                            const SizedBox(width: 16),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    filteredVehicleTypes[index].name,
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.green,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Text(
+                                    filteredVehicleTypes[index].info ?? '',
+                                    style: const TextStyle(fontSize: 16),
+                                  ),
+                                ],
+                              ),
                             ),
                           ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  );
+                },
+              )
+                  : const Center(
+                child: Text(
+                  'No se encontraron vehículos.',
+                  style: TextStyle(fontSize: 18),
                 ),
               ),
-            );
-          },
-        )
-            : const Center(
-          child: Text(
-            'No hay vehículos disponibles.',
-            style: TextStyle(fontSize: 18),
-          ),
+            ),
+          ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categorías',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_car),
+            label: 'Mis Alquileres',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green,
+        onTap: _onItemTapped, // Llama al método para manejar la navegación
       ),
     );
   }
